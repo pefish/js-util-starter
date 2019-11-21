6bdc5f6d460c7eb6ab56f0c802fe88ecfed1ae06
joy <joy.lai@zg.com>
2019-11-21 16:38:22 +0800

    fix

--- src/starter.ts
+++ src/starter.ts
@@ -1,17 +1,9 @@
-declare global {
-  namespace NodeJS {
-    interface Global {
-      logger: any,
-    }
-  }
-}
-
 export default class StarterUtil {
   static startAsync (method: () => Promise<void>, errCb: () => Promise<void> = null, exitWhenFinish: boolean = false): void {
     method().then(() => {
       exitWhenFinish && process.exit(0)
     }).catch(async (err) => {
-      global.logger.error(err)
+      console.error(err)
       errCb && (await errCb())
     })
   }
