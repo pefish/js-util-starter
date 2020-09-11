8ff5afaca0c422ce1052ccbbd309ccf6b3e44ef3
pefish <pefish@qq.com>
2020-09-11 13:46:09 +0800

    fix

--- src/starter.ts
+++ src/starter.ts
@@ -1,10 +1,11 @@
 export default class StarterUtil {
-  static startAsync (method: () => Promise<void>, errCb: () => Promise<void> = null, exitWhenFinish: boolean = false): void {
+  static startAsync (method: () => Promise<void>, exitWhenFinish: boolean = true, errCb: () => Promise<void> = null): void {
     method().then(() => {
       exitWhenFinish && process.exit(0)
     }).catch(async (err) => {
       console.error(err)
       errCb && (await errCb())
+      exitWhenFinish && process.exit(0)
     })
   }
 }
